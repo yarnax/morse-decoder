43593d92557990699549c389fd860e1aa6fca417
yarnax <feniks_63@mail.ru>
2020-10-20 13:58:08 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -38,14 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    console.log(expr)
+    //console.log(expr)
     console.log(expr.length)
     let slicedArray = [];
     for (i=0; i<(expr.length/10); i++) {
         slicedArray.push(expr.slice(i*10, (i+1)*10))
     }
-    console.log(slicedArray)
-    // write your solution here
+    //console.log(slicedArray)
+    slicedArray.forEach((letter, index) => {
+        if (letter==="**********") {
+            slicedArray[index]=" ";
+        } else {
+            let dashLetter = "";
+            for (i=0;i<5;i++) {
+                if (letter[i*2]==="0" && letter[i*2+1]==="0") {
+                    dashLetter +="";
+                }
+                if (letter[i*2]==="1" && letter[i*2+1]==="0") {
+                    dashLetter +=".";
+                }
+                if (letter[i*2]==="1" && letter[i*2+1]==="1") {
+                    dashLetter +="-";
+                }
+            }
+            slicedArray[index]=dashLetter;
+        }
+        
+        
+    })
+    //console.log(slicedArray)
+    let answer = "";
+    slicedArray.forEach((sign, index) => {
+        if (sign===" ") {
+            answer += " ";
+        } else {
+            answer += MORSE_TABLE[sign];
+        }
+        
+    })
+    console.log(answer)
+    return answer;
+
+
+
 }
 
 module.exports = {
